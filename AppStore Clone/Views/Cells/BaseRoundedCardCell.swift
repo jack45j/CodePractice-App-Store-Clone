e930d996940e74d3bb399eacbea6eda1904cd9fe
Benson Lin <jack45j@gmail.com>
2018-05-28 18:22:52 +0800

    Added cards shadow but still has some bounds issues.

--- AppStore Clone/Views/Cells/BaseRoundedCardCell.swift
+++ AppStore Clone/Views/Cells/BaseRoundedCardCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class BaseRoundedCardCell: UICollectionViewCell {
     
@@ -16,13 +17,18 @@ class BaseRoundedCardCell: UICollectionViewCell {
     /// is Pressed State
     private var isPressed: Bool = false
     
-    override func awakeFromNib() {
-        
+    /// Shadow View
+    private weak var ShadowView: UIView?
+    
+    /// Core Motion Manager
+    private let motionManager = CMMotionManager()
+    
+    enum Constants {
+        static let InnerMargin: CGFloat = 20.0
     }
     
     
-    /// CollectionView Cell highlight animation.
-    
+    /// CollectionView Cell highlight animation
     func animate(isHighlighted: Bool, completion: ((Bool) -> Void)?=nil) {
         if isHighlighted {
             UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [UIViewAnimationOptions.beginFromCurrentState], animations: {
@@ -48,5 +54,50 @@ class BaseRoundedCardCell: UICollectionViewCell {
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesCancelled(touches, with: event)
         self.animate(isHighlighted: false)
+    }
+}
+
+
+/// MARK: - shadow
+
+extension BaseRoundedCardCell {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureShadowView()
+    }
+    
+    private func configureShadowView() {
+        self.ShadowView?.removeFromSuperview()
+        let shadowView = UIView(frame: CGRect(x: Constants.InnerMargin,
+                                              y: Constants.InnerMargin,
+                                              width: bounds.width - (2 * Constants.InnerMargin),
+                                              height: bounds.height - (2 * Constants.InnerMargin)))
+        insertSubview(shadowView, at: 0)
+        self.ShadowView = shadowView
+        
+        // Roll/Pitch Dynamic Shadow
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.02
+            motionManager.startDeviceMotionUpdates(to: .main, withHandler: { (motion, error) in
+                if let motion = motion {
+                    let pitch = motion.attitude.pitch * 10 // x-axis
+                    let roll = motion.attitude.roll * 10 // y-axis
+                    self.applyShadow(width: CGFloat(roll), height: CGFloat(pitch))
+                }
+            })
+        }
+    }
+    
+    private func applyShadow(width: CGFloat, height: CGFloat) {
+        if let shadowView = ShadowView {
+            let shadowPath = UIBezierPath(roundedRect: shadowView.bounds, cornerRadius: 14.0)
+            shadowView.layer.masksToBounds = false
+            shadowView.layer.shadowRadius = 8.0
+            shadowView.layer.shadowColor = UIColor.black.cgColor
+            shadowView.layer.shadowOffset = CGSize(width: width, height: height)
+            shadowView.layer.shadowOpacity = 0.35
+            shadowView.layer.shadowPath = shadowPath.cgPath
+        }
     }
 }
